de0d5fa97dbb4c43c75b362f4bbb5cd54918ec8c
Arthur Picard <arthur.picard7@gmail.com>
2021-11-24 15:49:09 +0100

    Created: Show page for specific coin and coins controller created with show method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @coins = Coin.all
-  end
-
-  def show
-    @coin = Coin.find(params[:id])
-  end
-
-  def market
-    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    coins_serialized = URI.open(url).read
-    @coins = JSON.parse(coins_serialized)
   end
 end
