d9f2245092b3efe1a5c50753c679467662cb517f
Arthur Picard <arthur.picard7@gmail.com>
2021-11-24 13:01:36 +0100

    Update: Market css layout, number delimiter with comma

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ Portfolio.create!(
 
 require 'rest-client'
 
-url = RestClient.get 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+url = RestClient.get 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
 url_array = JSON.parse(url)
 
 url_array.each do |coin|
