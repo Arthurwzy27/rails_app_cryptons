d2f7e6cb23411eab010b568080cd6067a0900401
Arthur Picard <arthur.picard7@gmail.com>
2021-11-25 17:54:56 +0100

    Add: Graph JS added and portfolio entries css on the right side and entry card created, still some issue with coins_serialized from api

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,7 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :entries, dependent: :destroy
-  has_many :coins, through: :entries, dependent: :destroy
+  has_many :coins, through: :entries
 
   validates :name, presence: true
 end
