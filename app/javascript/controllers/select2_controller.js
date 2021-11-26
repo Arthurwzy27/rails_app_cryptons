a268f0df79cdb3b83677f425c513424a62a3ceea
Arthur Picard <arthur.picard7@gmail.com>
2021-11-26 14:31:38 +0100

    Update: Holding portfolio calculation, select2 font in black, P/L calculation, automatic buying price per crypto

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -25,12 +25,18 @@ import 'select2/dist/css/select2.css';
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["price", "input", "coins"]
+
   connect() {
     console.log('hello from select2 controller')
-    $(this.element).select2({
-      width: "100%",
-      theme: "classic",
-      color: "#000",
+    $(this.inputTarget).select2({
+      width: "100%"
+    });
+
+    $(this.inputTarget).on('select2:select', (e) => {
+      const coins = JSON.parse(this.coinsTarget.dataset.coins);
+      const selectedCoin = coins.find(element => element["name"] == e.params.data.text)
+      this.priceTarget.value = parseFloat(selectedCoin["price"])
     });
   }
 }
