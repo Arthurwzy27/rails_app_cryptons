16eb0d77c516db7d690d629d1af52935220d172a
Arthur Picard <arthur.picard7@gmail.com>
2021-11-29 17:42:29 +0100

    Feature: Add more information per coin as ATH, market cap, high24 low24 and some css with div

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_104324) do
+ActiveRecord::Schema.define(version: 2021_11_29_154010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_11_29_104324) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "price"
     t.text "description"
+    t.integer "market_cap_rank"
+    t.bigint "market_cap"
+    t.bigint "total_volume"
+    t.integer "high_24h"
+    t.integer "low_24h"
+    t.bigint "total_supply"
+    t.integer "ath"
+    t.integer "ath_change_percentage"
   end
 
   create_table "entries", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,13 @@ url_array.each do |coin|
     image_url: coin["image"],
     price: coin["current_price"],
     percentage_24: coin["price_change_percentage_24h"],
-    # history_seven:
+    market_cap_rank: coin["market_cap_rank"],
+    market_cap: coin["market_cap"],
+    total_volume: coin["total_volume"],
+    high_24h: coin["high_24h"],
+    low_24h: coin["low_24h"],
+    total_supply: coin["total_supply"],
+    ath: coin["ath"],
+    ath_change_percentage: coin["ath_change_percentage"]
   )
 end
